e6235de4b480e71e69ef9920d7c4fe258673b951
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-05-08 15:39:26 +0200

    Haladas FC

--- admin.js
+++ admin.js
@@ -92,33 +92,36 @@ function kiindul(){
     let leptet = 0;
     let s = "";
     for(let i = 0; i < listofprice.length; i++){
+        let zsindex = letezikparam(listofprice[i].id);
         let idgparam = idkWhyParam(listofprice[i].id);
-        if(leptet == 0){
-            s += `<div class="row">`;
-        }
-        s+=`<div class="card col-md-3 col-sm-6 col-smx-12">` +
-                `<a href="#" class="card-header text-success">${listofprice[i].termeknev}</a>` +
-                `<div class="card-body">` +
-                ` <img src="${convImageExist(listofprice[i].image)}" class="img-thumbnail" alt="">` +
-                    `<ul class = "">` +
-                        `<li>Szélesség: ${idgparam.szelesseg}cm</li>` +
-                        `<li>Hosszúság/Vastagság: ${idgparam.hosszusag}cm</li>` +
-                        `<li>Magasság: ${idgparam.magassag}cm</li>` +
-                        `<li>Anyag: ${idgparam.anyag}</li>` +
-                        `<li>Tömör: ${idgparam.tomor ? "Igen" : "Nem"}.</li>` +
-                    `</ul>` +
-                `</div>` +
-                `<div class="card-footer">` +
-                    `<div class="row">` +
-                        `<div class="float-md-start">Ár: ${listofprice[i].ar} Ft</div>` +
-                        ` <button class="float-md-end">Kosárba</button>` +
+        if(szuro(listofprice[i]) && szuroparam/*                */ */){
+            if(leptet == 0){
+                s += `<div class="row">`;
+            }
+            s+=`<div class="card col-md-3 col-sm-6 col-smx-12">` +
+                    `<a href="#" class="card-header text-success">${listofprice[i].termeknev}</a>` +
+                    `<div class="card-body">` +
+                    ` <img src="${convImageExist(listofprice[i].image)}" class="img-thumbnail" alt="">` +
+                        `<ul class = "">` +
+                            `<li>Szélesség: ${idgparam.szelesseg}${zsindex != -1 ? "cm": ""}</li>` +
+                            `<li>Hosszúság/Vastagság: ${idgparam.hosszusag}${zsindex !=-1 ? "cm": ""}</li>` +
+                            `<li>Magasság: ${idgparam.magassag}${zsindex != -1 ? "cm": ""}</li>` +
+                            `<li>Anyag: ${idgparam.anyag}</li>` +
+                            `<li>Tömör: ${idgparam.tomor ? "Igen" : "Nem"}.</li>` +
+                        `</ul>` +
                     `</div>` +
-                `</div>` +
-            `</div>`;
-        leptet++;
-        if(leptet == 4){
-            s += `</div>`
-            leptet = 0;
+                    `<div class="card-footer">` +
+                        `<div class="row">` +
+                            `<div class="float-md-start">Ár: ${listofprice[i].ar} Ft</div>` +
+                            ` <button class="float-md-end">Kosárba</button>` +
+                        `</div>` +
+                    `</div>` +
+                `</div>`;
+            leptet++;
+            if(leptet == 4){
+                s += `</div>`
+                leptet = 0;
+            }
         }
     }
     document.getElementsByClassName("termekek")[0].innerHTML = s;
@@ -218,7 +221,7 @@ function szamotKiir(szam){
 }
 
 function convImageExist(image_url){
-    return imageExists(image_url) ? image_url : "kepek/itsnotexist.png";
+    return imageExists(image_url) || image_url === "" ? image_url : "kepek/itsnotexist.png";
 }
 
 function imageExists(image_url){
@@ -226,6 +229,24 @@ function imageExists(image_url){
     http.open('HEAD', image_url, false);
     http.send();
     return http.status != 404;
+}
+
+function szuro(object={
+    termeknev: "Háromszög",
+    image: "n",
+    ar: 6000
+}){
+    return true;
+}
+
+function szuroparam(object={
+    szelesseg: 13,
+    magassag: 13,
+    hosszusag: 1,
+    anyag: "Fa",
+    tomor: true
+}){
+    return true;
 }
 
 //szures, rendezes
